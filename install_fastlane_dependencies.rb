2ab2d3df8307b910ea4d312faf99d3de9e167c5c
Thiago Lioy <lioy@Thiagos-MacBook-Air-2.local>
2015-02-03 11:34:58 -0200

    script to install fastlane tools

---
+++ install_fastlane_dependencies.rb
@@ -0,0 +1,25 @@
+def update_brew
+  system("brew update")
+end
+
+def install_deps
+  ["phantomjs"].map { |d| system("sudo gem install #{d} --verbose") }
+end
+
+def update_xcode_cmd_tools
+  system("xcode-select --install")
+end
+
+def install_fastlane_tools
+  ["fastlane","snapshot","frameit",
+    "produce","sigh","pem","deliver"].map { |d| system("sudo gem install #{d} --verbose") }
+end
+
+puts("Updating Homebrew ..")
+update_brew
+puts("Installing Dependencies ..")
+install_deps
+puts("Updating xcode cmd line tools ..")
+update_xcode_cmd_tools
+puts("Installing Fastlane tools ..")
+install_fastlane_tools
