7d48ea3b249a82b1bf0c0b25c198fe3a3ec1740f
Thiago Lioy <lioy@Thiagos-MacBook-Air-2.local>
2015-02-09 15:23:17 -0200

    update dependencies

--- install_calabash_dependencies.rb
+++ install_calabash_dependencies.rb
@@ -1,5 +1,5 @@
 def install_deps
-  ["calabash-common",'calabash-cucumber','cs-bdd'].map { |d| system("sudo gem install #{d} --verbose") }
+  ["calabash-common",'calabash-cucumber','cs-bdd','blabla'].map { |d| system("sudo gem install #{d} --verbose") }
 end
 puts("Installing Dependencies ..")
 install_deps
